b8b01c522dee9d0b14184a8ee7899a33bcd869e1
Martin <martinjonsson01@gmail.com>
2022-12-17 16:49:23 +0100

    Replace Number struct with indices and bool vec

--- src/multithreaded/rayon.rs
+++ src/multithreaded/rayon.rs
@@ -1,4 +1,3 @@
-use crate::Number;
 use rayon::prelude::*;
 
 /// Concurrently finds all of the prime numbers that exist up to the given limit.
@@ -15,44 +14,42 @@ use rayon::prelude::*;
 pub fn find_primes(search_up_to: i32) -> Vec<i32> {
     let cpus = num_cpus::get_physical();
     let numbers_per_thread = (search_up_to as usize) / cpus;
-    let mut numbers: Vec<Number> = (0..search_up_to + 1)
-        .into_par_iter()
-        .with_min_len(numbers_per_thread)
-        .map(|n| Number::new(n, true))
-        .collect();
-    numbers[0].is_prime = false;
-    numbers[1].is_prime = false;
-    let mut next_prime: Option<Number> = Some(numbers[2]);
+    let mut marks: Vec<bool> = (0..=search_up_to).map(|_| true).collect();
+    marks[0] = false;
+    marks[1] = false;
+    let mut next_prime: Option<i32> = Some(2_i32);
     while let Some(prime) = next_prime {
         // The multiples of p that are smaller than p^2 are already marked.
-        let start_multiple = prime.value.pow(2);
+        let start_multiple = prime.pow(2);
         if start_multiple > search_up_to {
             break;
         }
 
-        numbers
+        marks
             .par_iter_mut()
             .with_min_len(numbers_per_thread)
             .skip(start_multiple as usize)
-            .step_by(prime.value as usize)
-            .for_each(|number| number.is_prime = false);
+            .step_by(prime as usize)
+            .for_each(|is_prime| *is_prime = false);
 
-        next_prime = numbers
+        next_prime = marks
             .par_iter()
             .with_min_len(numbers_per_thread)
-            .find_first(|number| number.value > prime.value && number.is_prime)
-            .cloned();
+            .enumerate()
+            .find_first(|(number, &is_prime)| *number as i32 > prime && is_prime)
+            .map(|(number, _is_prime)| number as i32);
     }
-    collect_marked(numbers)
+    collect_marked(marks)
 }
 
-fn collect_marked(values: Vec<Number>) -> Vec<i32> {
+fn collect_marked(values: Vec<bool>) -> Vec<i32> {
     let cpus = num_cpus::get_physical();
     let numbers_per_thread = values.len() / cpus;
     values
         .par_iter()
         .with_min_len(numbers_per_thread)
-        .filter(|number| number.is_prime)
-        .map(|number| number.value)
+        .enumerate()
+        .filter(|(_number, &is_prime)| is_prime)
+        .map(|(number, _is_prime)| number as i32)
         .collect()
 }
--- src/singlethreaded/optimized.rs
+++ src/singlethreaded/optimized.rs
@@ -1,5 +1,3 @@
-use crate::Number;
-
 /// Finds all of the prime numbers that exist up to the given limit.
 ///
 /// `search_up_to` is an upper limit at which to stop the search.
@@ -12,36 +10,46 @@ use crate::Number;
 /// let primes: Vec<i32> = find_primes(1_000_000);
 /// ```
 pub fn find_primes(search_up_to: i32) -> Vec<i32> {
-    let mut numbers: Vec<Number> = (0..=search_up_to).map(|n| Number::new(n, true)).collect();
-    numbers[0].is_prime = false;
-    numbers[1].is_prime = false;
-    let mut next_prime: Option<Number> = Some(numbers[2]);
+    let marks = mark_primes(search_up_to);
+    collect_marked(marks)
+}
+
+pub(crate) fn mark_primes(search_up_to: i32) -> Vec<bool> {
+    let mut marks: Vec<bool> = (0..=search_up_to).map(|_| true).collect();
+    marks[0] = false;
+    marks[1] = false;
+    let mut next_prime = Some(2_i32);
     while let Some(prime) = next_prime {
         // The multiples of p that are smaller than p^2 are already marked.
-        let start_multiple = prime.value.pow(2);
+        let start_multiple = prime.pow(2);
         if start_multiple > search_up_to {
             break;
         }
 
-        numbers
+        marks
             .iter_mut()
             .skip(start_multiple as usize)
-            .step_by(prime.value as usize)
-            .for_each(|number| number.is_prime = false);
+            .step_by(prime as usize)
+            .for_each(|is_prime| *is_prime = false);
 
-        next_prime = numbers
+        next_prime = marks
             .iter()
-            .find(|number| number.value > prime.value && number.is_prime)
-            .cloned();
+            .enumerate()
+            .find(|(number, &is_prime)| *number as i32 > prime && is_prime)
+            .map(|(number, _is_prime)| number as i32);
     }
-    collect_marked_2(numbers)
+    marks
 }
 
-fn collect_marked_2(values: Vec<Number>) -> Vec<i32> {
-    values
-        .iter()
-        .filter(|number| number.is_prime)
-        .map(|number| number.value)
+fn collect_marked<Iter>(marks: Iter) -> Vec<i32>
+where
+    Iter: IntoIterator<Item = bool>,
+{
+    marks
+        .into_iter()
+        .enumerate()
+        .filter(|(_index, is_prime)| *is_prime)
+        .map(|(index, _is_prime)| index as i32)
         .collect()
 }
 
